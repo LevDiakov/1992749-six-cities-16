738ab1ac5d5ccc8347b05f6e980eb41045e2118e
LevDiakov <381208dl@gmail.com>
2024-08-20 21:01:34 -0300

    Добавит компонент Форма комментария

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,35 +1,15 @@
-import { Fragment, useEffect, useState } from 'react';
-import { AuthorizationStatus, COMMENT_LENGTH, NumericalValues, REVIEWS_RATING } from '../../const';
+import { AuthorizationStatus, NumericalValues } from '../../const';
 import { userReviews } from '../../types/types';
 import { gethumanizeDate, getSortedByDates } from '../../utils';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { addReviewsAction } from '../../store/api-actions';
-import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../store/hooks';
+import ReviewsForm from '../reviews-form/reviews-form';
 
 type ReviewsProps = {
   reviews: userReviews;
 }
 
 function Reviews({reviews}: ReviewsProps): JSX.Element {
-  const dispatch = useAppDispatch();
-  const {id} = useParams();
   const authorizationStatus = useAppSelector((state) => state.rental.authorizationStatus);
-  const [formData, setFormData] = useState({
-    rating: 0,
-    comment: '',
-  });
-  const validateForm = () =>
-    formData.rating && formData.comment.length >= COMMENT_LENGTH.min && formData.comment.length <= COMMENT_LENGTH.max;
-  const isDisabled = !validateForm();
-  const isFormUploading = useAppSelector((state) => state.rental.isReviewsUploadingStatus);
-  useEffect(() => {
-    if (!isFormUploading) {
-      setFormData({
-        rating: 0,
-        comment: '',
-      });
-    }
-  }, [isFormUploading]);
 
   return (
     <section className="offer__reviews reviews">
@@ -69,79 +49,7 @@ function Reviews({reviews}: ReviewsProps): JSX.Element {
         ))}
       </ul>
       {authorizationStatus === AuthorizationStatus.Auth ? (
-        <form className="reviews__form form" action="#" method="post"
-          onSubmit={(evt) => {
-            evt.preventDefault();
-            const message = {
-              offerId: id,
-              data: formData,
-            };
-            dispatch(addReviewsAction(message));
-          }}
-        >
-          <label className="reviews__label form__label" htmlFor="review">
-      Your review
-          </label>
-          <div className="reviews__rating-form form__rating" >
-            {REVIEWS_RATING.map(({title, rank}) => (
-              <Fragment key={title} >
-                <input
-                  className="form__rating-input visually-hidden"
-                  name="rating"
-                  value={rank}
-                  id={`${rank}-stars`}
-                  type="radio"
-                  onChange={
-                    (evt) => {
-                      const {name, value} = evt.target;
-                      const numberValue = Number.parseInt(value, 10);
-                      setFormData({...formData, [name]: numberValue});
-                    }
-                  }
-                />
-                <label key={title}
-                  htmlFor={`${rank}-stars`}
-                  className="reviews__rating-label form__rating-label"
-                  title={title}
-                >
-                  <svg className="form__star-image" width={37} height={33}>
-                    <use xlinkHref="#icon-star" />
-                  </svg>
-                </label>
-              </Fragment>
-            ))}
-          </div>
-          <textarea onChange={
-            (evt) => {
-              const {name, value} = evt.target;
-              setFormData({...formData, [name]: value});
-            }
-          }
-          value={formData.comment}
-          className="reviews__textarea form__textarea"
-          id="comment"
-          name="comment"
-          placeholder="Tell how was your stay, what you like and what can be improved"
-          minLength={COMMENT_LENGTH.min}
-          maxLength={COMMENT_LENGTH.max}
-          />
-          <div className="reviews__button-wrapper">
-            <p className="reviews__help">
-        To submit review please make sure to set{' '}
-              <span className="reviews__star">rating</span> and describe your stay
-        with at least <b className="reviews__text-amount">50 characters </b>
-        and no more than <b className="reviews__text-amount">300 characters</b>.<br/>
-        There are <b className="reviews__text-amount">{COMMENT_LENGTH.max - formData.comment.length} characters</b> left to use.
-            </p>
-            <button
-              className="reviews__submit form__submit button"
-              type="submit"
-              disabled={isDisabled}
-            >
-        Submit
-            </button>
-          </div>
-        </form>
+        <ReviewsForm />
       ) : null}
     </section>
 
